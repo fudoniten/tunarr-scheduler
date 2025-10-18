a6dd3ab2c9aeb47e530a656b2ece69c2f93ab5b7
niten <niten@fudo.org>
2025-10-17 20:27:59 -0700

    Customize JobRunner

--- src/tunarr/scheduler/jobs/runner.clj
+++ src/tunarr/scheduler/jobs/runner.clj
@@ -4,20 +4,19 @@
   (:import (java.time Instant)
            (java.util UUID)))
 
-(defrecord JobRunner [jobs]
-  java.io.Closeable
-  (close [_]
-    (reset! jobs {})))
+(defprotocol IJobRunner
+  (jobs [self])
+  (add-job! [self job-id job])
+  (job-info [self job-id])
+  (get-job [self job-id])
+  (list-jobs [self])
+  (submit! [self config job-fn])
+  (close [self]))
 
-(defn create
-  "Create a new job runner instance."
-  [_]
-  (->JobRunner (atom {})))
-
-(defn- now []
+(defn now []
   (Instant/now))
 
-(defn- format-ts [^Instant inst]
+(defn format-ts [^Instant inst]
   (when inst
     (.toString inst)))
 
@@ -33,16 +32,15 @@
     (contains? #{:succeeded :failed} status) (assoc :result result)
     (= :failed status) (assoc :error error)))
 
-(defn- update-job! [jobs id f & args]
-  (apply swap! jobs update id f args))
+(defmulti update-job! class)
 
-(defn job-info
+#_(defn job-info
   "Fetch public information about a job."
   [runner job-id]
   (when-let [job (get @(.jobs runner) job-id)]
     (->public-job job)))
 
-(defn list-jobs
+#_(defn list-jobs
   "Return public information about all known jobs ordered by creation time."
   [runner]
   (->> @(.jobs runner)
@@ -51,45 +49,71 @@
        (map ->public-job)
        vec))
 
-(defn submit!
+(defn submit-job!
   "Submit an asynchronous job. Returns the initial job information.
 
   The job-config map must include a :type keyword describing the job. Optional
   :metadata will be stored alongside the job record."
-  ([runner job-type task-fn]
-   (submit! runner {:type job-type} task-fn))
-  ([runner {:keys [type metadata] :as job-config} task-fn]
-   (when-not type
-     (throw (ex-info "Job type is required" {:job-config job-config})))
-   (let [job-id (str (UUID/randomUUID))
-         created-at (now)
-         job {:id job-id
-              :type type
-              :status :queued
-              :metadata metadata
-              :created-at created-at}]
-     (swap! (.jobs runner) assoc job-id job)
-     (future
-       (let [start (now)]
-         (update-job! (.jobs runner) job-id merge {:status :running
-                                                   :started-at start})
-         (try
-           (let [result (task-fn)
-                 finished (now)]
-             (update-job! (.jobs runner) job-id merge {:status :succeeded
-                                                       :completed-at finished
-                                                       :result result}))
-           (catch Throwable t
-             (log/error t "Job failed" {:job-id job-id :type type})
-             (let [finished (now)]
-               (update-job! (.jobs runner) job-id merge {:status :failed
-                                                         :completed-at finished
-                                                         :error {:message (.getMessage t)
-                                                                 :type (.getName (class t))}}))))))
-     (job-info runner job-id))))
+  [runner {:keys [type metadata] :as job-config} task-fn]
+  (when-not type
+    (throw (ex-info "Job type is required" {:job-config job-config})))
+  (let [job-id (str (UUID/randomUUID))
+        update-progress (fn [progress] (update-job! runner job-id merge {:progress progress}))]
+    (add-job! runner job-id
+              {:id job-id
+               :type type
+               :status :queued
+               :metadata metadata
+               :created-at (now)})
+    (future
+      (update-job! runner job-id merge {:status :running :started-at (now)})
+      (try
+        (let [result (task-fn update-progress)]
+          (update-job! runner job-id merge {:status :succeeded
+                                            :completed-at (now)
+                                            :result result}))
+        (catch Throwable t
+          (log/error t "Job failed" {:job-id job-id :type type})
+          (update-job! runner job-id merge {:status :failed
+                                            :completed-at (now)
+                                            :error {:message (.getMessage t)
+                                                    :type (.getName (class t))
+                                                    :error t}}))))
+    (job-info runner job-id)))
 
 (defn shutdown!
   "Clear all tracked jobs."
   [runner]
-  (.close runner)
+  (close runner)
   nil)
+
+(defrecord JobRunner [jobs]
+  IJobRunner
+  (jobs [_] @jobs)
+  (add-job! [_ job-id job]
+    (apply swap! jobs assoc job-id job))
+  (get-job [_ job-id]
+    (get @jobs job-id nil))
+  (job-info [self job-id]
+    (when-let [job  (get-job self job-id)]
+      (->public-job job)))
+  (list-jobs [_]
+    (->> @jobs
+         (vals)
+         (sort-by :created-at #(compare %2 %1))
+         (map ->public-job)
+         (vec)))
+  (submit! [self config task-fn]
+    (submit-job! self config task-fn))
+    
+  java.io.Closeable
+  (close [_] (reset! jobs {})))
+
+(defn create
+  "Create a new job runner instance."
+  [_]
+  (->JobRunner (atom {})))
+
+(defmethod update-job! tunarr.scheduler.jobs.runner.JobRunner
+  [runner id f & args]
+  (apply swap! (:jobs runner) update id f args))
