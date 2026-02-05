a28775012d840ca2da067ae8ca9f73b370d9643e
niten <niten@fudo.org>
2026-02-04 21:51:37 -0800

    Switch to using jellyfin-sidekick for tag updates
    Replace broken Jellyfin POST /Items/{itemId} API with jellyfin-sidekick
    service which writes NFO files and triggers Jellyfin refresh.
    
    Changes:
    - Removed direct Jellyfin update API calls
    - Added sidekick-request function to call jellyfin-sidekick API
    - Updated update-item-tags! to POST to /api/items/{itemId}/tags
    - Simplified sync logic (no need to GET item first)
    - Default sidekick URL: http://jellyfin-sidekick.arr.svc.cluster.local:8080
    - Configurable via :sidekick-url in collection config
    
    This works around Jellyfin Issue #10724 by using NFO files instead of
    the broken metadata update API. Tags flow: tunarr-scheduler ->
    jellyfin-sidekick -> NFO files -> Jellyfin refresh -> ErsatzTV.

--- src/tunarr/scheduler/media/jellyfin_sync.clj
+++ src/tunarr/scheduler/media/jellyfin_sync.clj
@@ -1,129 +1,67 @@
 (ns tunarr.scheduler.media.jellyfin-sync
-  "Sync tags from catalog to Jellyfin using HTTP API"
+  "Sync tags from catalog to Jellyfin via jellyfin-sidekick service"
   (:require [clj-http.client :as http]
             [cheshire.core :as json]
             [clojure.string :as str]
-            [clojure.pprint :refer [pprint]]
             [cemerick.url :as url]
             [camel-snake-kebab.core :refer [->PascalCaseString]]
             [taoensso.timbre :as log]
             [tunarr.scheduler.media :as media]
             [tunarr.scheduler.media.catalog :as catalog]))
 
-(defn jellyfin-authenticated-request
-  "Make an authenticated request to Jellyfin"
-  [{:keys [api-key] :as config} method url & {:keys [body query-params]}]
-  (when-not api-key
-    (log/error "No API key found in config!" {:config-keys (keys config)})
-    (throw (ex-info "Jellyfin API key not configured" {:config config})))
-  (let [opts (cond-> {:headers {"X-Emby-Token" api-key
-                                "Content-Type" "application/json"}
+(def tag-keyword->string ->PascalCaseString)
+
+(defn- sidekick-request
+  "Make a request to jellyfin-sidekick service"
+  [sidekick-url method path & {:keys [body]}]
+  (let [url (str (url/url sidekick-url path))
+        opts (cond-> {:headers {"Content-Type" "application/json"}
                       :throw-exceptions false}
-               body (assoc :body (json/generate-string body))
-               query-params (assoc :query-params query-params))]
-    (log/debug "Jellyfin request" {:method method :url url :has-api-key (boolean api-key)})
+               body (assoc :body (json/generate-string body)))]
+    (log/debug "Jellyfin-sidekick request" {:method method :url url})
     (case method
       :get (http/get url opts)
       :post (http/post url opts)
       (throw (ex-info "Unsupported HTTP method" {:method method})))))
 
-(defn- format-guid
-  "Format a hex string as a UUID with dashes.
-  Jellyfin expects UUIDs in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  [id-string]
-  (let [cleaned (str/replace id-string #"-" "")]
-    (if (= 32 (count cleaned))
-      (format "%s-%s-%s-%s-%s"
-              (subs cleaned 0 8)
-              (subs cleaned 8 12)
-              (subs cleaned 12 16)
-              (subs cleaned 16 20)
-              (subs cleaned 20 32))
-      id-string)))
-
-(defn- build-item-url
-  "Build the URL for updating a Jellyfin item"
-  [base-url item-id]
-  (str (url/url base-url "Items" (format-guid item-id))))
-
-(defn- build-search-url
-  "Build URL for searching items"
-  [base-url & {:keys [params]}]
-  (-> (url/url base-url "Items")
-      (assoc :query params)
-      str))
-
-(defn- get-item
-  "Get the full item data from Jellyfin by searching for it by ID.
-   Uses the Items search endpoint with the item ID as a filter.
-   Gets ALL fields to ensure we have a complete item for updating."
-  [config item-id]
-  (let [guid (format-guid item-id)
-        ;; Don't specify Fields parameter to get all fields
-        search-url (build-search-url (:base-url config) 
-                                     :params {:Ids guid})]
-    (log/debug "Searching for Jellyfin item" {:item-id item-id :url search-url})
-    (let [response (jellyfin-authenticated-request config :get search-url)]
+(defn update-item-tags!
+  "Update tags for a single Jellyfin item via jellyfin-sidekick"
+  [sidekick-url item-id tags]
+  (let [tag-strings (mapv tag-keyword->string tags)
+        path (str "api/items/" item-id "/tags")]
+    (log/info "Updating Jellyfin item tags via sidekick" {:item-id item-id :tags tag-strings})
+    
+    (let [response (sidekick-request sidekick-url :post path 
+                                    :body {:tags tag-strings})]
       (if (= 200 (:status response))
-        (when-let [result (json/parse-string (:body response) true)]
-          (when-let [items (:Items result)]
-            (first items)))
         (do
-          (log/error "Failed to search for item in Jellyfin" 
-                    {:item-id item-id 
+          (log/info "Successfully updated tags for item" {:item-id item-id})
+          {:success true :item-id item-id})
+        (do
+          (log/error "Failed to update tags for item"
+                    {:item-id item-id
                      :status (:status response)
                      :body (:body response)})
-          nil)))))
-
-(def tag-keyword->string ->PascalCaseString)
-
-(defn update-item-tags!
-  "Update tags for a single Jellyfin item"
-  [config item-id tags]
-  (let [url (build-item-url (:base-url config) item-id)
-        tag-strings (mapv tag-keyword->string tags)]
-    (log/info "Updating Jellyfin item tags" {:item-id item-id :tags tag-strings})
-    ;; First, GET the current item data
-    (if-let [current-item (get-item config item-id)]
-      (let [;; Update the Tags field with new tags
-            updated-item (assoc current-item :Tags tag-strings)]
-        (log/debug "Retrieved item" {:item-id item-id 
-                                      :current-tags (:Tags current-item)
-                                      :item-keys (keys current-item)})
-        (log/debug "Sending update to Jellyfin" {:item-id item-id 
-                                                  :url url
-                                                  :new-tags (:Tags updated-item)})
-        (let [response (jellyfin-authenticated-request config :post url :body updated-item)]
-          (if (= 204 (:status response))
-            (do
-              (log/info "Successfully updated tags for item" {:item-id item-id})
-              {:success true :item-id item-id})
-            (do
-              (log/error "Failed to update tags for item"
-                        {:item-id item-id
-                         :status (:status response)
-                         :body (:body response)})
-              (log/debug (format "Request:\n%s" (with-out-str (pprint updated-item))))
-              {:success false
-               :item-id item-id
-               :error (:body response)
-               :status (:status response)}))))
-      (do
-        (log/error "Failed to get item from Jellyfin" {:item-id item-id})
-        {:success false
-         :item-id item-id
-         :error "Item not found in Jellyfin"}))))
+          {:success false
+           :item-id item-id
+           :error (:body response)
+           :status (:status response)})))))
 
 (defn sync-library-tags!
-  "Sync all tags from catalog to Jellyfin for a given library.
-  
+  "Sync all tags from catalog to Jellyfin via jellyfin-sidekick.
+   
    Iterates over all media items in the catalog for the specified library
-   and pushes their current tags to Jellyfin, replacing all existing tags."
+   and pushes their current tags to jellyfin-sidekick, which writes NFO files
+   and triggers Jellyfin to refresh metadata."
   [catalog config library opts]
   (let [{:keys [report-progress]} opts
+        sidekick-url (or (:sidekick-url config)
+                        "http://jellyfin-sidekick.arr.svc.cluster.local:8080")
         media-items (catalog/get-media-by-library catalog library)
         total-count (count media-items)
-        _ (log/info (format "Syncing tags for %d items in library %s" total-count library))
+        _ (log/info (format "Syncing tags for %d items in library %s via jellyfin-sidekick" 
+                           total-count library))
+        _ (log/info "Using jellyfin-sidekick at" {:url sidekick-url})
         results (atom {:synced 0 :failed 0 :errors []})]
     
     (doseq [[idx item] (map-indexed vector media-items)]
@@ -135,7 +73,7 @@
                            :total total-count
                            :item item-id}))
         
-        (let [result (update-item-tags! config item-id tags)]
+        (let [result (update-item-tags! sidekick-url item-id tags)]
           (if (:success result)
             (swap! results update :synced inc)
             (do
